f02ee83d9a006a582f9b5aa8cd445f456acbf995
Roshani9731 <84122787+Roshani9731@users.noreply.github.com>
2023-02-11 10:07:12 +0530

    nthFibonacci done

---
+++ Recursion/nthFibonacci.cpp
@@ -0,0 +1,10 @@
+long long int nthFibonacci(long long int n){
+        // code here
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return nthFibonacci(n-1)+nthFibonacci(n-2);
+ }
